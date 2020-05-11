3b2e462e2a0bf05ce29813abea1dc97c1e00a87c
Richard van Brunschot <richardvanbrunschot@gmail.com>
2020-05-11 16:11:11 +0200

    updated footer link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="footer">
-      <p>Design &amp; Code by Richard van Brunschot</p>
+      <a href="https://github.com/Richardvbr">Design &amp; Code by Richard van Brunschot</a>
     </div>
   </div>
 </template>
@@ -22,7 +22,6 @@ $text-color: #333;
   .footer {
     font-family: 'Calibre', sans-serif;
     background-color: #fff;
-    color: $text-color;
     height: 100%;
     font-size: 15px;
     box-shadow: 0px -10px 15px 1px rgba(0,0,0,0.1);
@@ -30,6 +29,11 @@ $text-color: #333;
     justify-content: center;
     align-items: center;
     padding-top: 5px;
+
+    a {
+      color: $text-color;
+      text-decoration: none;
+    }
   }
 }
 </style>
