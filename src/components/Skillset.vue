a0f7b4b77298b0c2c6fe431f662974000d28ace6
Richard van Brunschot <richardvanbrunschot@gmail.com>
2021-01-16 13:00:53 +0100

    Update Skillset.vue

--- src/components/Skillset.vue
+++ src/components/Skillset.vue
@@ -7,27 +7,25 @@
     </p>
     <div class="card-skillset">
       <div class="card-skillset__interface">
-        <h3>User interface</h3>
+        <h3>User interface and tools</h3>
         <ul class="interface-list">
-          <li class="interface-list__item"></li>
-          <li class="interface-list__item"></li>
-          <li class="interface-list__item"></li>
-          <li class="interface-list__item"></li>
-          <li class="interface-list__item"></li>
-          <li class="interface-list__item"></li>
-          <li class="interface-list__item"></li>
+          <li class="interface-list__item">HTML5, CSS3, SCSS, React styled-components</li>
+          <li class="interface-list__item">Experience with responsive and mobile-first design</li>
+          <li class="interface-list__item">Basic UX/UI skills, good eye for detail</li>
+          <li class="interface-list__item">Experience with Bootstrap 5</li>
+          <li class="interface-list__item">Basic understanding of Tailwind CSS</li>
+          <li class="interface-list__item">Familiar with SEO principles</li>
+          <li class="interface-list__item">VS Code, Git, Adobe Suite, Wordpress</li>
         </ul>
       </div>
       <div class="card-skillset__javascript">
         <h3>JavaScript</h3>
         <ul class="javascript-list">
-          <li class="javascript-list__item"></li>
-          <li class="javascript-list__item"></li>
-          <li class="javascript-list__item"></li>
-          <li class="javascript-list__item"></li>
-          <li class="javascript-list__item"></li>
-          <li class="javascript-list__item"></li>
-          <li class="javascript-list__item"></li>
+          <li class="javascript-list__item">JavaScript ES6+</li>
+          <li class="javascript-list__item">Experience with React.js and Vue.js</li>
+          <li class="javascript-list__item">Displaying data fetched from an API</li>
+          <li class="javascript-list__item">Creating single-page applications</li>
+          <li class="javascript-list__item">Familiar with npm, webpack, eslint</li>
         </ul>
       </div>
     </div>
@@ -52,91 +50,141 @@ $accent-color: #038AFF;
   max-width: 1050px;
   margin: 0 auto;
   font-family: 'Inter', sans-serif;
-  font-size: 17px;
-  font-weight: 500;
+  font-size: 16px;
+  font-weight: 400;
   color: $text-color;
 
-    h2 {
-      font-size: 2rem;
-      margin-bottom: 3rem;
-      color: #333;
-      position: relative;
-      padding-bottom: 0.4rem;
-      font-family: 'Inter', sans-serif;
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 3rem;
+    color: #333;
+    position: relative;
+    padding-bottom: 0.4rem;
+    font-family: 'Inter', sans-serif;
 
-      &:after {
-        content: "";
-        width: 65px;
-        height: 4px;
-        background-color: $accent-color;
-        position: absolute;
-        bottom: 0;
-        left: 0;
+    &:after {
+      content: "";
+      width: 65px;
+      height: 4px;
+      background-color: $accent-color;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
+  }
+
+  .description{
+    width: 550px;
+    padding-bottom: 10px;
+  }
+
+  .card-skillset {
+    width: 75%;
+    height: 23rem;
+    background-color: #fff;
+    box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.2);
+    display: flex;
+    padding: 1.5rem 3rem 1.5rem 3rem;
+
+    &__interface {
+      width: 40ch;
+    }
+
+    ul {
+    padding: 0 2rem;
+    list-style: square;
+
+      li {
+        margin: 1rem 0;
       }
     }
-
-    .description{
-      width: 550px;
-      padding-bottom: 10px;
-    }
-
-    .card-skillset {
-      width: 75%;
-      height: 30rem;
-      background-color: #fff;
-      box-shadow: 0px 0px 15px 1px rgba(0,0,0,0.2);
-      display: flex;
-      padding: 1.5rem 3rem 1.5rem 3rem;
-
-      &__javascript {
-        margin-left: 30%;
-      }
-    }
+  }
 }
 
 @media screen and (max-width: 700px){
   #skillset-container {
-    margin-top: 5rem;
-    max-width: 100vw;
-    padding: 0 2rem;
+    padding-top: 200px;
+    max-width: 1050px;
+    margin: 0 2rem;
+    font-family: 'Inter', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    color: $text-color;
 
+    .description{
+      width: 100%;
+      padding-bottom: 10px;
+    }
 
-    .flex-container {
+    .card-skillset {
+      width: 100%;
+      height: 41rem;
       display: flex;
       flex-direction: column;
-      justify-content: center;
+      padding: 1.5rem 2rem 1.5rem 2rem;
 
-      .description{
-        text-align: left;
-        width: 100%;
-        padding-bottom: 10px;
-      } 
+      &__interface {
+        width: 25ch;
+      }
+
+      h3 {
+        font-weight: bold;
+        font-size: 1.25rem;
+      }
+
+      ul {
+      padding: 0 1rem;
+      list-style: square;
+
+        li {
+          margin: 1rem 0;
+        }
+      }
     }
-  } 
+  }
 }
+
 
 @media screen and (min-width: 701px) and (max-width: 1049px){
   #skillset-container {
-    max-width: 100vw;
-    padding: 0 2rem;
-    font-size: 22px;
-    margin-top: 5rem;
+    padding-top: 200px;
+    max-width: 1050px;
+    margin: 0 2rem;
+    font-family: 'Inter', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    color: $text-color;
 
-    #skillset {
-      scroll-margin-top: -60px;
+    .description{
+      width: 100%;
+      padding-bottom: 10px;
     }
 
-    .flex-container {
+    .card-skillset {
+      width: 100%;
+      height: 28rem;
       display: flex;
-      flex-direction: column;
-      justify-content: center;
+      padding: 1.5rem 2rem 1.5rem 2rem;
 
-      .description{
-        text-align: left;
-        width: 80%;
-        padding-bottom: 10px;
-      } 
+      &__interface {
+        width: 35ch;
+      }
+
+      h3 {
+        font-weight: bold;
+        font-size: 1.25rem;
+      }
+
+      ul {
+      padding: 0 1rem;
+      list-style: square;
+      font-size: 1.1rem;
+
+        li {
+          margin: 1rem 0;
+        }
+      }
     }
-  } 
+  }
 }
 </style>
