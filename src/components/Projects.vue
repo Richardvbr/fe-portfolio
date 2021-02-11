dd64dcb4dd04ef9cb47b397c03c3fdb32991428a
Richard van Brunschot <richardvanbrunschot@gmail.com>
2021-02-11 08:35:38 +0100

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -41,7 +41,7 @@ export default {
           imgAlt: 'Netflix React App',
           url: 'https://netflix-react-app-fd806.web.app/',
           title: 'Netlix React App',
-          software: ['React.js', 'Firebase', 'Axios', 'CSS'],
+          software: ['React.js', 'Redux', 'Firebase', 'Axios', 'CSS'],
           description: "Netflix UI clone built with React.js (including Redux and React Router). Fetches data from TMDB API. Single-page, component-based application. Drag-to-scroll poster rows.",
           note: "Work in progress: authentication (sign-up, sign-in/out) using Firebase.",
           github: 'https://github.com/Richardvbr/netflix-react-app'
